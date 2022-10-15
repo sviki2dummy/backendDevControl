a4d6152f274d2cbd88e2759dabba3e9f6aa554c6
KristianK1 <kikihd11@gmail.com>
2022-10-15 02:00:05 +0200

    changed wss for express api

--- index.js
+++ index.js
@@ -1,43 +1,52 @@
 const webSocketsServerPort = process.env.PORT || 8000;
-const webSocketServer = require('websocket').server;
-const http = require('http');
+//const webSocketServer = require('websocket').server;
+//const http = require('http');
 
-const server = http.createServer();
-server.listen(webSocketsServerPort);
-console.log('listening on port ' + webSocketsServerPort);
+const express = require('express');
+const app = express();
 
-const wsServer = new webSocketServer({
-  httpServer: server,
-});
+app.get('/',(req,res) => {
+  res.send('hello world');
+})
 
+app.listen(webSocketsServerPort);
 
-const clients = {};
+// const server = http.createServer();
+// server.listen(webSocketsServerPort);
+// console.log('listening on port ' + webSocketsServerPort);
 
-const getUniqueID = () => {
-  const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-  return s4() + s4() + '-' + s4();
-};
-
-wsServer.on('request', function (request) {
-  var userID = getUniqueID();
-  console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
-
-  const connection = request.accept(null, request.origin);
-  clients[userID] = connection;
-  console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+// const wsServer = new webSocketServer({
+//   httpServer: server,
+// });
 
 
-  connection.on('message', function (message) {
-    if (message.type === 'utf8') {
-      console.log('Received Message: ', message.utf8Data);
+// const clients = {};
 
-      // broadcasting message to all connected clients
-      for (key in clients) {
-        clients[key].sendUTF(message.utf8Data);
-        // console.log('sent Message to: ', clients[key]);
-      }
-    }
-  })
-});
+// const getUniqueID = () => {
+//   const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+//   return s4() + s4() + '-' + s4();
+// };
+
+// wsServer.on('request', function (request) {
+//   var userID = getUniqueID();
+//   console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
+
+//   const connection = request.accept(null, request.origin);
+//   clients[userID] = connection;
+//   console.log('connected: ' + userID + ' in ' + Object.getOwnPropertyNames(clients));
+
+
+//   connection.on('message', function (message) {
+//     if (message.type === 'utf8') {
+//       console.log('Received Message: ', message.utf8Data);
+
+//       // broadcasting message to all connected clients
+//       for (key in clients) {
+//         clients[key].sendUTF(message.utf8Data);
+//         // console.log('sent Message to: ', clients[key]);
+//       }
+//     }
+//   })
+// });
 
 
