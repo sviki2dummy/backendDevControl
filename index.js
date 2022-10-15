6dd89f33aa29f8a51d2758409b4e396d4a36efed
KristianK1 <kikihd11@gmail.com>
2022-10-15 03:46:18 +0200

    changed port to process.env.PORT

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// const port = process.env.PORT || 8000;  //heroku ili localhost
-const port = '0.0.0.0' || 8000; //render ili localhost
+const port = process.env.PORT || 8000;  //heroku ili localhost
+// const port = '0.0.0.0' || 8000; //render ili localhost
 
 
 const express = require('express');
