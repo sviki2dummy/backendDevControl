b1dcebcd8f5b826cfc63e72d8fd0016126bafd89
KristianK1 <kikihd11@gmail.com>
2022-10-15 03:41:20 +0200

    console log on request

--- index.js
+++ index.js
@@ -6,8 +6,9 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res) => {
+  console.log('request:/');
   res.send('hello world!!!');
-})
+});
 
 app.listen('0.0.0.0', () => {
   console.log('started listening on portt ' + port)
