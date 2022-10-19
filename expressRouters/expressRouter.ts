1a926409e2ba6fba4ad21f96a8bb840977e598e0
KristianK1 <kikihd11@gmail.com>
2022-10-20 01:36:41 +0200

    added .ts to imports

--- expressRouters/expressRouter.ts
+++ expressRouters/expressRouter.ts
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 
-const userAuthRouter = require('./userAuths/userAuthRouter');
+const userAuthRouter = require('./userAuths/userAuthRouter.ts');
 router.use('/userAuth', userAuthRouter);
 
 //add more use-s
--- expressRouters/userAuths/userAuthRouter.ts
+++ expressRouters/userAuths/userAuthRouter.ts
@@ -1,13 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const loginUserAuthRouter = require('./subrouter/loginRouter');
+const loginUserAuthRouter = require('./subrouter/loginRouter.ts');
 router.use('/login', loginUserAuthRouter);
 
-const registerUserAuthRouter = require('./subrouter/registerRouter');
+const registerUserAuthRouter = require('./subrouter/registerRouter.ts');
 router.use('/register', registerUserAuthRouter);
 
-const logoutUserAuthRouter = require('./subrouter/logoutRouter');
+const logoutUserAuthRouter = require('./subrouter/logoutRouter.ts');
 router.use('/logout', logoutUserAuthRouter);
 
 module.exports = router;
